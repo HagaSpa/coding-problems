5cd5c51fff4e45af61554ae740d8b8799ab6fc6b
HagaSpa <justicesparrow13@gmail.com>
2020-09-14 09:57:21 +0900

    solved leetcode problem. word-ladder.

---
+++ leetcode/word-ladder/main.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import defaultdict
+from collections import deque
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        d = defaultdict(list)
+        l = len(beginWord)
+        for word in wordList:
+            for i in range(l):
+                d[word[:i] + "*" + word[i+1:]].append(word)
+        
+        q = deque()
+        q.append((beginWord, 1))
+        chked = {beginWord: True}
+        
+        while q:
+            k,v = q.popleft()
+            for i in range(l):
+                w = k[:i] + "*" + k[i+1:]
+                for word in d[w]:
+                    if word == endWord:
+                        return v+1
+                    if word not in chked:
+                        chked[word] = True
+                        q.append((word, v+1))
+                d.pop(w)
+        return 0
+
+
+if __name__ == "__main__":
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot","dot","dog","lot","log","cog"]
+    s = Solution()
+    ans = s.ladderLength(beginWord=beginWord, endWord=endWord, wordList=wordList)
+    print(ans)
