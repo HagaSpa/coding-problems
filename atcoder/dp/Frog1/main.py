9112998d5441ebc42709a25ca01592a2af77b8a2
HagaSpa <justicesparrow13@gmail.com>
2020-10-17 11:03:17 +0900

    initialize dp array

--- atcoder/dp/Frog1/main.py
+++ atcoder/dp/Frog1/main.py
@@ -4,10 +4,9 @@ from typing import List
 def calc_min_cost(n: int, h: List[int]) -> int:
     dp = [float('inf')]*n
     dp[0] = 0
+    dp[1] = abs(h[0]-h[1])
+
     for i in range(1,n):
-        if i==1:
-            dp[i] = abs(h[i]-h[i-1])
-            continue
         skip_one = dp[i-1] + abs(h[i]-h[i-1])
         skip_two = dp[i-2] + abs(h[i]-h[i-2])
         dp[i] = min(skip_one, skip_two)
